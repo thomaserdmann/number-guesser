079f3064a3fa548233f1b8d3aceddf0fb51b505f
Thomas Erdmann <thomas@innofy.nl>
2021-08-29 23:19:11 +0200

    adds CompareGuesses Function

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ This function should return a random integer between 0 and 9. */
 const generateTarget = () => {
     return Math.floor(Math.random() * 9);
 };
+
+/*Create a compareGuesses() function. This function will be called each round to determine which guess is closest to the target number.
+This function:
+Has three parameters representing the user (human) guess, a computer guess, and the secret target number to be guessed.
+Determines which player (human or computer) wins based on which guess is closest to the target. If both players are tied, the human user should win.
+Return true if the human player wins, and false if the computer player wins. */
+
+const compareGuesses = (user, computer, target) => {
+    if ((Math.abs(target - computer)) < (Math.abs(target - user))) return false;
+    else return true;
+};
+
+console.log(compareGuesses(7, 2, 4))
