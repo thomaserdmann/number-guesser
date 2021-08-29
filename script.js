70b537e55d16d11e2d1956fa6a9c07b8e3d0e285
Thomas Erdmann <thomas@innofy.nl>
2021-08-29 23:18:54 +0200

    Add generateTarget function

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+/* Create a generateTarget() function. This function will be called at the start of each new round in order to generate the new secret target number.
+This function should return a random integer between 0 and 9. */
 
+const generateTarget = () => {
+    return Math.floor(Math.random() * 9);
+};
